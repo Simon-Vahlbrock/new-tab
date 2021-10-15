4647570e268e2bc148b3710799495a45d6bb6381
Simon <simon.vahlbrock@tobit.com>
2021-10-15 17:25:07 +0200

    🚧

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import img1 from '../utils/images/1.jpg'
 import img2 from '../utils/images/2.jpg'
 import img3 from '../utils/images/3.jpg'
@@ -8,7 +8,6 @@ import img5 from '../utils/images/5.jpg'
 function App() {
     const rand = (max, min) => Math.floor(Math.random() * (max - min + 1))
     const image = () => {
-        console.log(rand(4,0))
         switch (rand(4, 0)) {
             case 0:
                 return `url(${img1})`
@@ -20,13 +19,20 @@ function App() {
                 return `url(${img4})`
             case 4:
                 return `url(${img5})`
+            default:
+                return `url(${img1})`
         }
     }
-
-
     return (
         <div className="app" style={{backgroundImage: image()}}>
-            Hallo
+            <div className='app__flex'>
+                <div className='app__heading'>
+                    <p className='app__heading'>New Tab</p>
+                </div>
+            </div>
+            <div className='app__flex'>
+                <input/>
+            </div>
         </div>
     );
 }
